88aebf620356b182eb6dee7a6227dd822a769b8c
Jeremy Irvine <jeremylukeirvine@gmail.com>
2020-12-21 13:32:26 -0800

    Updates

---
+++ precache-manifest.80f45c175561b615be2269c55a856806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da0316914ae07eca25cf7ac5ad932f21",
+    "url": "/jeremyirvine.github.io/hacker-news/index.html"
+  },
+  {
+    "revision": "ca89389f9caead743749",
+    "url": "/jeremyirvine.github.io/hacker-news/static/css/main.37f0afbc.chunk.css"
+  },
+  {
+    "revision": "d405fdd074f4318ccc1f",
+    "url": "/jeremyirvine.github.io/hacker-news/static/js/2.e5fb3032.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jeremyirvine.github.io/hacker-news/static/js/2.e5fb3032.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca89389f9caead743749",
+    "url": "/jeremyirvine.github.io/hacker-news/static/js/main.0855a445.chunk.js"
+  },
+  {
+    "revision": "0d0056edf0ce28b093a0",
+    "url": "/jeremyirvine.github.io/hacker-news/static/js/runtime-main.06ac9f80.js"
+  }
+]);
